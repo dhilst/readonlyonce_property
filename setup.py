3b49fa0a0491e20c4cf6ab98f4d71ac2c5cd71c8
Daniel Hilst Selli <daniel@versatushpc.com.br>
2018-02-02 11:25:07 -0200

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     license='Apache 2.0',
     author='Daniel Hilst Selli <danielhilst@gmail.com>',
+    url='https://github.com/dhilst/readonlyonce_property',
     py_modules='readonlyonce_property',
     test_suite='readonlyonce_property.test',
 )
