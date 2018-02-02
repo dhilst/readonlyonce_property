6a220a5c34e1aa3899af08e77b03b1384f87d491
Daniel Hilst Selli <daniel@versatushpc.com.br>
2018-02-02 11:38:28 -0200

    Add description to setup.py.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='readonlyonce_property',
     version='1.0.0',
     license='Apache 2.0',
+    description='@readonlyonce_property decorator that cache its return value.',
     author='Daniel Hilst Selli <danielhilst@gmail.com>',
     url='https://github.com/dhilst/readonlyonce_property',
     py_modules='readonlyonce_property',
